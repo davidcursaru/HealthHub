3b45c26376b9ffc36690140e7142a74e971517b0
David <david_cursaru@yahoo.com>
2024-01-12 16:22:03 +0200

    Created structure for sleep tracker with mat cards

--- health-hub-client/src/app/components/sleep-tracker/sleep-tracker.component.ts
+++ health-hub-client/src/app/components/sleep-tracker/sleep-tracker.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, inject } from '@angular/core';
+import { map } from 'rxjs';
+
 
 @Component({
   selector: 'app-sleep-tracker',
@@ -6,5 +9,72 @@ import { Component } from '@angular/core';
   styleUrls: ['./sleep-tracker.component.css']
 })
 export class SleepTrackerComponent {
+  
+
+
+  private breakpointObserver = inject(BreakpointObserver);
+  cards = this.breakpointObserver.observe([
+    Breakpoints.Small,
+    Breakpoints.Handset,
+    Breakpoints.Medium,
+    Breakpoints.Large,
+    Breakpoints.XLarge
+  ]).pipe(
+    map(({ breakpoints }) => {
+      if (breakpoints[Breakpoints.Small] || breakpoints[Breakpoints.Handset]) {
+        return [
+          { title: 'Schedule sleep hours ', cols: 1, rows: 3, route: '' },
+          { title: 'Sleep score', cols: 1, rows: 3, route: '' },
+          { title: 'Asleep time', cols: 1, rows: 8, route: '' },
+          { title: 'Deep sleep', cols: 1, rows: 6, route: '' },
+          { title: 'Awake', cols: 1, rows: 6, route: '' },
+          { title: 'Regularity', cols: 1, rows: 6, route: '' },
+          { columns: 1 }
+        ];
+      }
+      else if (breakpoints[Breakpoints.Medium]) {
+        return [
+          { title: 'Schedule sleep hours ', cols: 1, rows: 3, route: '' },
+          { title: 'Sleep score', cols: 1, rows: 3, route: '' },
+          { title: 'Asleep time', cols: 1, rows: 8, route: '' },
+          { title: 'Deep sleep', cols: 1, rows: 6, route: '' },
+          { title: 'Awake', cols: 1, rows: 6, route: '' },
+          { title: 'Regularity', cols: 1, rows: 6, route: '' },
+          { columns: 1 }
+        ];
+      }
+      else if (breakpoints[Breakpoints.Large] || breakpoints[Breakpoints.XLarge]) {
+        return [
+          { title: 'Schedule sleep hours ', cols: 1, rows: 20, route: '' },
+          { title: 'Sleep score', cols: 1, rows: 20, route: '' },
+          { title: 'Asleep time', cols: 1, rows: 20, route: '' },
+          { title: 'Deep sleep', cols: 1, rows: 20, route: '' },
+          { title: 'Awake', cols: 1, rows: 20, route: '' },
+          { title: 'Regularity', cols: 1, rows: 43, route: '' },
+          { title: 'Sleep phases', cols: 2, rows: 23, route: '' },
+          { columns: 3 }
+        ];
+      }
+
+      return [
+        { title: 'Schedule sleep hours ', cols: 1, rows: 3, route: '' },
+        { title: 'Sleep score', cols: 1, rows: 3, route: '' },
+        { title: 'Asleep time', cols: 1, rows: 8, route: '' },
+        { title: 'Deep sleep', cols: 1, rows: 6, route: '' },
+        { title: 'Awake', cols: 1, rows: 6, route: '' },
+        { title: 'Regularity', cols: 1, rows: 6, route: '' },
+        { columns: 1 }
+      ];
+
+    })
+  );
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+  }
+
 
 }
